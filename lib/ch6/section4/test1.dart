161d1e609abc2e1b7a558be187c86ca8331b312e
juu124 <juu12424@gmail.com>
2026-04-22 14:41:33 +0900

    [실습] Stream과 StreamBuilder를 통해서 연속적인 데이터와 위젯 사용하기

---
+++ lib/ch6/section4/test1.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // 특정 업무가 진행되는 함수.. 이 함수가 반복적으로 호출이 되어야 한다고 가정한다.
+  int calFun(int x) {
+    return x * x;
+  }
+
+  Stream<int> test() {
+    Duration duration = Duration(seconds: 3);
+    Stream<int> stream = Stream<int>.periodic(
+        duration, calFun); // calFun 함수를 3초에 한번씩 호출하고 그 결과를 스트림으로 리턴한다.
+    return stream.take(5);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('stream test'),),
+          body: StreamBuilder( // stream 에 의해 반복적으로 발생되는 데이터로 화면 구성하기 위한 위젯
+            stream: test(),
+            builder: (context, snapShot) { // 최초 호출 그리고 반복적으로 호출된다.
+              if (snapShot.connectionState == ConnectionState.done) {
+                // 데이터 발행이 완료되었다면..
+                return Text('completed');
+              } else if (snapShot.connectionState == ConnectionState.waiting) {
+                // 데이터 발행을 대기하고 있는 상태..
+                return SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircularProgressIndicator(),
+                );
+              }
+              // 발행중..
+              return Text('${snapShot.data}');
+            },
+          ),
+        )
+    );
+  }
+}
