5552acba3166d49b2f6ae13ba4f6594ee2b8ebae
juu124 <juu12424@gmail.com>
2026-04-23 11:21:34 +0900

    [실습] MultiProvider를 사용해서 여러개의 Provider를 등록

--- lib/ch7/section2/test1.dart
+++ lib/ch7/section2/test1.dart
@@ -13,6 +13,26 @@ class Counter with ChangeNotifier {
   }
 }
 
+// 클래스 Counter를 참조해서 만들어야하는 상태가 있을 수 있다.
+// provider를 이용해 공개해야하는 상태 데이터.. 다른 상태 데이터 참조해서
+// 이때 Sum 상태를 등록하려면 ProxyProvider를 사용하면된다.
+class Sum {
+  int _sum = 0;   // _ : 외부에서 접근할 수 없음
+
+  int get sum => _sum;
+
+  void set sum(value) {
+    _sum = 0;
+    for (var i = 1; i <= value; i++) {
+      _sum += i;
+    }
+  }
+
+  Sum(Counter counter) {
+    sum = counter.count;
+  }
+}
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -21,8 +41,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('provider test')),
-        body: ChangeNotifierProvider<Counter>.value(
-          value: Counter(),
+        body: MultiProvider(
+            providers: [
+              ChangeNotifierProvider<Counter>.value(
+                value: Counter(),
+              ),
+              Provider<int>.value(value: 10,),
+              Provider<String>.value(value: 'hello',),
+              // ProxyProvider 제네릭 타입 첫 번째 : 내가 받아야하는 상태 타입
+              // ProxyProvider 제네릭 타입 두 번째 : 내가 발행하는 상태 타입
+              ProxyProvider<Counter, Sum> (
+                // model : 내가 참조해야하는 다른 상태
+                // sum : 내가 이전에 발행했던 객체
+                update: (context, counter, sum) {
+                  if (sum != null) {
+                    sum.sum = counter.count;
+                    return sum;
+                  } else {
+                    return Sum(counter);
+                  }
+                },
+              ),
+            ],
           child: SubWidget(),
         ),
       ),
@@ -36,6 +76,11 @@ class SubWidget extends StatelessWidget {
     // 상위에 선언된 provider 상태 획득... 타입으로 식별해서..
     var counter = Provider.of<Counter>(context);
 
+    var int_data = Provider.of<int>(context);
+    var string_data = Provider.of<String>(context);
+
+    var sum = Provider.of<Sum>(context);
+
     return Column(
       children: [
         Text('provider counter : ${counter.count}'),
@@ -45,6 +90,9 @@ class SubWidget extends StatelessWidget {
           },
           child: Text('increment'),
         ),
+        Text('provider int : $int_data'),
+        Text('provider string : $string_data'),
+        Text('provider sum : ${sum.sum}'),
       ],
     );
   }
